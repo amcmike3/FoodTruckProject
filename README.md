17a086bbe8efed432bbb1ad8c1fe1285ee780dbf
Amcmike3 <amcmichael3@gmail.com>
2022-10-28 19:19:13 -0600

    final copy

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Food Truck Project
 
 ## Description
-- This application will prompt a user to enter a food truck name genre and rating up to 5 trucks
-- if the user enters quit on the food truck name they will be prompted with another menu where they can display information based on the inputs given. like average rating, list of food trucks
+- This application will prompt a user to enter a food truck name, food served, and rating up to 5 food trucks.
+- If the user enters quit on the food truck name they will be prompted with another menu where they can display information based on the inputs given. like average rating, list of food trucks, highest rated food truck, or quit entire program.
+- Special use case that had to be handled: when a user immediately quits application before entering any food trucks. 
 
-## Technologies
- - Java
- - GitHub 
- 
+## Limitations: 
+- program cannot handle errors like null pointer exceptions (although it is highly unlikely that would happen) or a more likely scenario where a user enters the wrong data type at the prompt. 
+- Program is limited to 5 food trucks.
+- does not store any data long-term (once program finished all data is lost).
+
+
+## Java concepts required for this project
+- for loops
+- while loops
+- if-else statements
+- methods
+- static vs. non-static
+- classes
+- object instances
+- encapsulation
+- getters and setters
+- constructors
+- toString
--- src/com/skilldistillery/foodtrucks/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtrucks/app/FoodTruckApp.java
@@ -8,7 +8,6 @@ public class FoodTruckApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Welcome to the food truck app!!");
 		String foodType = "";
 		int rating = -1;
 		int menuChoice = 0;
@@ -16,8 +15,10 @@ public class FoodTruckApp {
 		FoodTruck[] foodTruckList = new FoodTruck[5];
 		FoodTruckApp foodTruckAppObject = new FoodTruckApp();
 
+		System.out.println("Welcome to the food truck app!!");
 		System.out.println("Please enter food truck name:");
 		String input = scanner.nextLine();
+		// loop until user enters quit or 5 trucks are entered
 		while (!(input.equalsIgnoreCase("quit")) && index <= 4) {
 
 			System.out.println("Enter the food type that " + input + " serves:");
@@ -26,21 +27,22 @@ public class FoodTruckApp {
 			System.out.println("Please enter the rating for " + input + ":");
 			rating = scanner.nextInt();
 			scanner.nextLine();
+			//make sure user enters correct rating range
 			while (rating < 0 || rating > 5) {
 				System.out.println("Oops we rate on a scale of 0-5 please input another rating");
 				rating = scanner.nextInt();
 				scanner.nextLine();
-				
+
 			}
 
 			FoodTruck truck = new FoodTruck(input, foodType, rating);
 			foodTruckList[index] = truck;
 
 			index++;
-			if (index <= 4 ) {
+			if (index <= 4) {
 				System.out.println("Please enter food truck name or quit to see menu:");
 				input = scanner.nextLine();
-				
+
 			}
 
 		}
@@ -52,16 +54,21 @@ public class FoodTruckApp {
 			}
 			foodTruckAppObject.menu();
 			menuChoice = scanner.nextInt();
-			
+
 			if (menuChoice == 1) {
+
 				foodTruckAppObject.printList(foodTruckList);
+
 			} else if (menuChoice == 2) {
+
 				foodTruckAppObject.printAverage(foodTruckList);
-				
+
 			} else if (menuChoice == 3) {
+
 				foodTruckAppObject.printMax(foodTruckList);
-			
+
 			} else if (menuChoice == 4) {
+
 				System.out.println("GoodBye");
 				break;
 			} else {
@@ -74,7 +81,7 @@ public class FoodTruckApp {
 	}
 
 	private void printList(FoodTruck[] foodTruckList) {
-		
+
 		for (FoodTruck truck : foodTruckList) {
 			if (truck != null) {
 				System.out.println(truck);
@@ -85,6 +92,7 @@ public class FoodTruckApp {
 	private void printMax(FoodTruck[] foodTruckList) {
 		int index = 0;
 		int max = 0;
+		FoodTruck winner = foodTruckList[0];
 		String nameMax = "None";
 		for (FoodTruck truck : foodTruckList) {
 			if (truck == null) {
@@ -94,27 +102,29 @@ public class FoodTruckApp {
 			if (truck.getRating() > max) {
 				max = truck.getRating();
 				nameMax = truck.getName();
+				winner = truck;
 			}
 		}
 		System.out.println("The highest rated food truck is: " + nameMax + " with a rating of: " + max);
-		
+		System.out.println("Heres the rest of its info if you're interested.. \n" + winner);
+
 	}
 
 	private void printAverage(FoodTruck[] foodTruckList) {
-		
+
 		int index = 0;
 		int ratingSum = 0;
 		for (FoodTruck truck : foodTruckList) {
 			if (truck != null) {
 				ratingSum += truck.getRating();
 				index++;
-			}else {
+			} else {
 				break;
 			}
 		}
-		double average = ratingSum / index; 
+		double average = ratingSum / index;
 		System.out.println("average rating of the food trucks is: " + average);
-		
+
 	}
 
 	private void menu() {
@@ -122,7 +132,7 @@ public class FoodTruckApp {
 		System.out.println("Enter 2 to see the average rating of all food trucks");
 		System.out.println("Enter 3 to see the highest rated food truck");
 		System.out.println("Enter 4 to quit program");
-		
+
 	}
 
 }
